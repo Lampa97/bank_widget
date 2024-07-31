b772004d261f0a542db9d8dc09b6603f1a424ec1
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-07-31 16:33:16 +0300

    refactored masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,13 +3,9 @@ from typing import Optional
 
 def get_mask_card_number(card_number: str) -> Optional[str]:
     """Функция возвращает замаскированный номер банковской карты в формате 'XXXX XX** **** XXXX'"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-    return None
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> Optional[str]:
     """Функция возвращает замаскированный номер банковского счета в формате '**XXXX'"""
-    if account_number.isdigit() and len(account_number) == 20:
-        return f"**{account_number[-4:]}"
-    return None
+    return f"**{account_number[-4:]}"
