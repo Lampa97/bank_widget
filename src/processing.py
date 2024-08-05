1fb8b57a94382d3449136bb9bc10a6934e4967ec
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-05 21:37:18 +0300

    added sort_by_date func

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,12 @@ from typing import Iterable
 
 
 def filter_by_state(operation_data: Iterable[dict], state="EXECUTED") -> Iterable[dict]:
-    """Функция принимает список словарей и опционально значение для ключа state(по умолчанию 'EXECUTED').
-    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует
-    указанному значению."""
-    return [item for item in operation_data if item["state"] == state]
+    """Функция принимает список словарей с данными о транзакции и опционально значение для ключа state(по умолчанию
+    'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению."""
+    return [item for item in operation_data if item.get("state") == state]
+
+def sort_by_date(operation_data: Iterable[dict], ascending=True) -> Iterable[dict]:
+    """Функция принмает список словарей с данными о транзакции и возвращает новый список, отсортированный по дате.
+    По умолчанию - убывание."""
+    return sorted(operation_data, key=lambda x: x.get("date"), reverse=ascending)
