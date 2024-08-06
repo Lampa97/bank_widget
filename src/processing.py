56c83164f04da60feee3c66c2521d681b9e4d1d3
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-06 09:04:24 +0300

    fixed type annotation - processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,14 @@
-from typing import Iterable
+from typing import List
 
 
-def filter_by_state(operation_data: Iterable[dict], state="EXECUTED") -> Iterable[dict]:
+def filter_by_state(operation_data: List[dict], state: str = "EXECUTED") -> List[dict]:
     """Функция принимает список словарей с данными о транзакции и опционально значение для ключа state(по умолчанию
     'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению."""
     return [item for item in operation_data if item.get("state") == state]
 
-def sort_by_date(operation_data: Iterable[dict], ascending=True) -> Iterable[dict]:
-    """Функция принмает список словарей с данными о транзакции и возвращает новый список, отсортированный по дате.
+
+def sort_by_date(operation_data: List[dict], ascending: bool = True) -> List[dict]:
+    """Функция принимает список словарей с данными о транзакции и возвращает новый список, отсортированный по дате.
     По умолчанию - убывание."""
     return sorted(operation_data, key=lambda x: x.get("date"), reverse=ascending)
