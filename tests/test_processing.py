91e8958a62c0e6f0d9e1dd70017eaaa218ca2a35
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-07 20:08:36 +0300

    created tests and test report

---
+++ tests/test_processing.py
@@ -0,0 +1,44 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state_executed(processing_data_valid):
+    assert filter_by_state(processing_data_valid) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_state_canceled(processing_data_valid):
+    assert filter_by_state(processing_data_valid, state="CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_by_state_missing(processing_data_invalid):
+    assert filter_by_state(processing_data_invalid) == [{"id": 41428829, "state": "EXECUTED"}]
+
+
+def test_filter_by_date_ascending(processing_data_valid):
+    assert sort_by_date(processing_data_valid) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_filter_by_date_descending(processing_data_valid):
+    assert sort_by_date(processing_data_valid, ascending=False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+def test_filter_by_date_missing(processing_data_invalid):
+    with pytest.raises(TypeError):
+        assert sort_by_date(processing_data_invalid)
