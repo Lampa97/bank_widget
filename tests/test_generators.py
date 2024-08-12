016fa20acb4721304e3e4d031888ebccde46db91
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-12 18:31:00 +0300

    added test_generators.py

---
+++ tests/test_generators.py
@@ -0,0 +1,18 @@
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+import pytest
+
+def test_filter_by_currency_valid():
+    pass
+
+
+
+
+
+def transaction_descriptions_valid():
+    pass
+
+
+
+def card_number_generator_valid():
+    pass
