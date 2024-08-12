74152c12dc260aab659a2af7d9d295bf98e5af18
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-12 15:00:33 +0300

    created generators.py

---
+++ src/generators.py
@@ -0,0 +1,10 @@
+from typing import List, Iterator, Generator
+
+def filter_by_currency(transactions: List[dict]) -> Iterator:
+    pass
+
+def transaction_descriptions(transactions: List[dict]) -> Generator:
+    pass
+
+def card_number_generator(start: int, stop: int) -> Generator:
+    pass
--- src/processing.py
+++ src/processing.py
@@ -11,4 +11,4 @@ def filter_by_state(operation_data: List[dict], state: str = "EXECUTED") -> List
 def sort_by_date(operation_data: List[dict], ascending: bool = True) -> List[dict]:
     """Функция принимает список словарей с данными о транзакции и возвращает новый список, отсортированный по дате.
     По умолчанию - убывание."""
-    return sorted(operation_data, key=lambda x: x.get("date"), reverse=ascending)
+    return sorted(operation_data, key=lambda x: x["date"], reverse=ascending)
