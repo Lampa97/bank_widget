5200a208d15eec97737ff4b4b4fa34b22c87f3fe
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-13 09:25:07 +0300

    tests for generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,14 +1,17 @@
 from typing import List, Iterator, Generator
 
+
 def filter_by_currency(transactions: List[dict], currency: str) -> Iterator:
     """Функция принимает на вход список словарей, представляющих транзакции.
     Возвращает транзакции отфильтрованные по заданной валюте."""
     return filter(lambda x: x['operationAmount']['currency']['code'] == currency, transactions)
 
+
 def transaction_descriptions(transactions: List[dict]) -> Generator:
     """Функция принимает на вход список словарей с транзакциями.
     Возвращает описание каждой операции."""
-    return (x['description'] for x in transactions)
+    for data in transactions:
+        yield data['description']
         
 
 def card_number_generator(start: int, stop: int) -> Generator:
@@ -18,5 +21,4 @@ def card_number_generator(start: int, stop: int) -> Generator:
         new_number = base_number + n
         card_number = str(new_number)
         yield f'{card_number[1:5]} {card_number[5:9]} {card_number[9:13]} {card_number[13:]}'
-
 
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,17 +2,35 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 import pytest
 
-def test_filter_by_currency_valid():
-    pass
+def test_filter_by_currency_usd(transactions, usd_transactions):
+    expected = usd_transactions
+    result = list(filter_by_currency(transactions, 'USD'))
+    assert result == expected
+
+
+def test_filter_by_currency_rub(transactions, rub_transactions):
+    expected = rub_transactions
+    result = list(filter_by_currency(transactions, 'RUB'))
+    assert result == expected
 
 
 
 
 
-def transaction_descriptions_valid():
-    pass
+def test_transaction_descriptions(transactions):
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+    with pytest.raises(StopIteration):
+        assert next(generator)
 
 
 
-def card_number_generator_valid():
-    pass
+def test_card_number_generator():
+    generator = card_number_generator(12345005, 12345010)
+    assert next(generator) == '0000 0000 1234 5005'
+    assert next(generator) == '0000 0000 1234 5006'
+    assert next(generator) == '0000 0000 1234 5007'
