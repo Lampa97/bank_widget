c42369b0710d43d44bad1fe5d337bf182d2b7fa4
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-15 12:40:40 +0300

    added test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,5 @@
-from time import time
-from typing import Any, Callable
 from functools import wraps
+from typing import Any, Callable
 
 
 def log(filename: str = "console") -> Callable:
@@ -9,15 +8,12 @@ def log(filename: str = "console") -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
-                start_time = time()
                 result = func(*args, **kwargs)
-                end_time = time()
-                run_time = end_time - start_time
-                message = f"{func.__name__} ok. Runtime: {run_time}"
+                message = f"{func.__name__} ok."
             except Exception as e:
                 message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
                 result = None
-            if '.txt' in filename:
+            if ".txt" in filename:
                 with open(filename, "a", encoding="utf-8") as file:
                     file.write(f"\n{message}")
             else:
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,20 @@
 import pytest
 
 from src.decorators import log
+
+
+@log()
+def slice(string):
+    return string[1:]
+
+
+def test_log_success(capsys):
+    slice("abc")
+    captured = capsys.readouterr()
+    assert captured.out == f"slice ok.\n"
+
+
+def test_log_error(capsys):
+    slice(5678)
+    captured = capsys.readouterr()
+    assert captured.out == f"slice error: 'int' object is not subscriptable. Inputs: (5678,), {{}}\n"
