026941187a0182e12228e3a593fe6b4dced5662e
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-15 11:40:56 +0300

    decorators.py - added annotations

--- src/decorators.py
+++ src/decorators.py
@@ -1,23 +1,29 @@
 from time import time
+from typing import Any, Callable
+from functools import wraps
 
 
-def log(filename=''):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
+def log(filename: str = "") -> Callable:
+
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 start_time = time()
                 result = func(*args, **kwargs)
                 end_time = time()
                 run_time = end_time - start_time
-                message = f'{func.__name__} ok. Runtime: {run_time}'
+                message = f"{func.__name__} ok. Runtime: {run_time}"
             except Exception as e:
-                message = f'{func.__name__} error: {e}. Inputs: {args}, {kwargs}'
+                message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
                 result = None
             if filename:
-                with open(filename, 'a', encoding='utf-8') as file:
+                with open(filename, "a", encoding="utf-8") as file:
                     file.write(f"\n{message}")
             else:
                 print(message)
             return result
+
         return wrapper
+
     return decorator
