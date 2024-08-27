3fcc76eaabcebe8b4ac78be098f7c9624310b8cc
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-27 16:43:46 +0300

    test_utils.py and html report done

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,13 @@
-from unittest.mock import Mock
-
-import pytest
-
 from src.utils import get_transaction_data
+
+
+def test_get_transaction_data_error():
+    assert get_transaction_data("") == []
+
+
+def test_get_transaction_data(json_data):
+    assert get_transaction_data("../tests/test_operations.json") == json_data
+
+
+def test_get_transaction_data_empty():
+    assert get_transaction_data("../tests/test_empty_operations.json") == []
