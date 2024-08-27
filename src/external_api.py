744d0a3b0461ffbde6baa695b4fd2f7a28d973d3
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-27 13:42:32 +0300

    external_api done

---
+++ src/external_api.py
@@ -0,0 +1,23 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('API_KEY')
+headers= {
+  "apikey": API_TOKEN
+}
+
+def transaction_into_rub(transaction: dict) -> float:
+    currency = transaction['operationAmount']['currency']['code']
+    amount = transaction['operationAmount']['amount']
+    if currency == 'RUB':
+        return amount
+    elif currency == 'USD' or currency == 'EUR':
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            result = response.json()
+            return round(result['result'], 2)
