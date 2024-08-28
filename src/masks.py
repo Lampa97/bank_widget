4b98e6238329cf3e15dad28a51a8aa4ab5a10978
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-08-28 18:43:54 +0300

    logger for masks.py created

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,21 @@
+import logging
 from typing import Optional
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler("../logs/masks.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def get_mask_card_number(card_number: str) -> Optional[str]:
     """Функция возвращает замаскированный номер банковской карты в формате 'XXXX XX** **** XXXX'"""
+    logger.info(f"Маскируем номер карты {card_number}")
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> Optional[str]:
     """Функция возвращает замаскированный номер банковского счета в формате '**XXXX'"""
+    logger.info(f"Маскируем номер счета {account_number}")
     return f"**{account_number[-4:]}"
