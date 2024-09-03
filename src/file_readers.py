33c835dd74b71fad3085617ade7a5527ddcc2ed7
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2024-09-03 13:19:13 +0300

    test_file_readers done

--- src/file_readers.py
+++ src/file_readers.py
@@ -1,18 +1,18 @@
-import csv
+import os
 from typing import List
 
 import pandas as pd
 
 
 def read_csv_file(path: str) -> List[dict]:
-    """Функция позволяет читать информацию из csv-файла"""
-    info = pd.read_csv(path, delimiter=";")
-    result = info.to_dict(orient="records")
-    return result
+    """Функция позволяет читаь информацию из csv-файла"""
+    my_path = os.path.join(os.path.dirname(__file__), path)
+    info = pd.read_csv(my_path, delimiter=";")
+    return info.to_dict(orient="records")
 
 
 def read_excel_file(path: str) -> List[dict]:
     """Функция позволяет читать информацию из excel-файла"""
-    info = pd.read_excel(path)
-    result = info.to_dict(orient="records")
-    return result
+    my_path = os.path.join(os.path.dirname(__file__), path)
+    info = pd.read_excel(my_path)
+    return info.to_dict(orient="records")
